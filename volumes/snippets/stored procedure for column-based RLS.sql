b2c667f0552ba9968bd78e772c715a9362bf9c47
Imanol57864 <alfonsochoxd@gmail.com>
2026-07-01 17:38:32 -0600

    updated snippet

--- volumes/snippets/stored procedure for column-based RLS.sql
+++ volumes/snippets/stored procedure for column-based RLS.sql
@@ -10,9 +10,9 @@ BEGIN
     INTO v_area FROM "Area_Usuario" WHERE user_id = auth.uid() LIMIT 1;
 
     CASE v_area
-    WHEN 1 THEN RETURN QUERY SELECT row_to_json(v) FROM catanalisis_full v;
-    WHEN 2 THEN RETURN QUERY SELECT row_to_json(v) FROM catanalisis_full v;
-    WHEN 3 THEN RETURN QUERY SELECT row_to_json(v) FROM catanalisis_area3 v;
+    WHEN 1 THEN RETURN QUERY SELECT row_to_json(v) FROM catanalisis_full v ORDER BY v.codigo_completo;
+    WHEN 2 THEN RETURN QUERY SELECT row_to_json(v) FROM catanalisis_full v ORDER BY v.codigo_completo;
+    WHEN 3 THEN RETURN QUERY SELECT row_to_json(v) FROM catanalisis_area3 v ORDER BY v.codigo_completo;
     -- WHEN 4
     ELSE RAISE EXCEPTION 'El usuario no tiene permisos para consultar los análisis.';
     END CASE;
